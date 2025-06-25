17f52c0974461c7a9abd051ebe1632d9cfdf7067
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-06-25 18:56:44 +0200

    Add a basic CLI and storage functions

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,216 @@
-use serde::Serialize;
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 use tree_ds::prelude::*;
 
-#[derive(Debug, Clone, Serialize, Eq, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
 pub struct GitMetadata {
     pub revision: String,
 }
 
-#[derive(Debug, Clone, Serialize, Eq, PartialEq)]
-pub struct Metadata {
+#[derive(Debug, Clone, Deserialize, Serialize, Eq, PartialEq)]
+pub struct StateMetadata {
     pub title: String,
     pub git: Option<GitMetadata>,
 }
 
-#[derive(Debug, Clone, Serialize, Eq, PartialEq)]
-pub struct DecisionMetadata {
+#[derive(Debug, Clone, Deserialize, Serialize, Eq, PartialEq)]
+pub struct ActionMetadata {
     pub title: String,
 }
 
-#[derive(Debug, Clone, Serialize, Eq, PartialEq)]
-pub enum NodeType {
-    Decision(DecisionMetadata),
-    State(Metadata),
+#[derive(Debug, Clone, Serialize, Eq, PartialEq, Deserialize)]
+pub enum Metadata {
+    Action(ActionMetadata),
+    State(StateMetadata),
 }
 
-type N = Node<AutomatedId, NodeType>;
+type NodeType = Node<AutomatedId, Metadata>;
+type TreeType = Tree<AutomatedId, Metadata>;
 
-fn add_decision(
-    tree: &mut Tree<AutomatedId, NodeType>,
-    parent: &AutomatedId,
+fn add_action_node(
+    tree: &mut TreeType,
+    parent_id: &AutomatedId,
     title: &str,
-) -> AutomatedId {
+) -> Result<AutomatedId> {
     tree.add_node(
-        N::new_with_auto_id(Some(NodeType::Decision(DecisionMetadata {
+        NodeType::new_with_auto_id(Some(Metadata::Action(ActionMetadata {
             title: title.to_string(),
         }))),
-        Some(parent),
+        Some(parent_id),
     )
-    .unwrap()
 }
 
-fn add_state(
-    tree: &mut Tree<AutomatedId, NodeType>,
-    parent: &AutomatedId,
+fn add_state_node(
+    tree: &mut TreeType,
+    parent_id: &AutomatedId,
     title: &str,
     git_metadata: Option<GitMetadata>,
-) -> AutomatedId {
+) -> Result<AutomatedId> {
     tree.add_node(
-        N::new_with_auto_id(Some(NodeType::State(Metadata {
+        NodeType::new_with_auto_id(Some(Metadata::State(StateMetadata {
             title: title.to_string(),
             git: git_metadata,
         }))),
-        Some(parent),
+        Some(parent_id),
     )
-    .unwrap()
 }
 
-fn main() {
-    let mut tree: Tree<AutomatedId, NodeType> = Tree::new(Some("My tree"));
+#[derive(thiserror::Error, Debug)]
+enum LoadSaveError {
+    #[error("I/O error")]
+    Io(#[from] std::io::Error),
+    #[error("JSON error")]
+    Json(#[from] serde_json::Error),
+    #[error("Tree error")]
+    Tree,
+}
 
-    let root_state = Metadata {
+fn load_tree_from_file(file_path: &PathBuf) -> std::result::Result<TreeType, LoadSaveError> {
+    let file_content = std::fs::read_to_string(file_path)?;
+    serde_json::from_str(&file_content).map_err(|_| LoadSaveError::Tree)
+}
+
+fn save_tree_to_file(
+    tree: &TreeType,
+    file_path: &PathBuf,
+) -> std::result::Result<(), LoadSaveError> {
+    if let Some(parent) = file_path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+    let json_content = serde_json::to_string(tree)?;
+    std::fs::write(file_path, json_content)?;
+    Ok(())
+}
+
+fn get_dummy_tree(project_name: &str) -> Result<TreeType> {
+    let mut tree: TreeType = Tree::new(Some(project_name));
+
+    let root_state = StateMetadata {
         title: "Toilet clogged".to_string(),
         git: None,
     };
-    let root = tree
-        .add_node(N::new_with_auto_id(Some(NodeType::State(root_state))), None)
-        .unwrap();
+    let root = tree.add_node(
+        NodeType::new_with_auto_id(Some(Metadata::State(root_state))),
+        None,
+    )?;
 
-    let flush = add_decision(&mut tree, &root, "Flush toilet");
-    let _overflow = add_state(&mut tree, &flush, "overflow", None);
+    let flush = add_action_node(&mut tree, &root, "Flush toilet")?;
+    let _overflow = add_state_node(&mut tree, &flush, "overflow", None)?;
 
-    let pump = add_decision(&mut tree, &root, "pump it up");
-    let _unclogged = add_state(&mut tree, &pump, "unclogged", None);
+    let pump = add_action_node(&mut tree, &root, "pump it up")?;
+    let _unclogged = add_state_node(&mut tree, &pump, "unclogged", None)?;
+    Ok(tree)
+}
 
-    println!("Hello, world!");
+/// Debug Tree CLI
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Project name
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Parser, Debug)]
+struct ProjectArgs {
+    /// Projects directory
+    #[arg(long, default_value = "./debug-tree")]
+    dir: PathBuf,
+    /// Project name
+    project: String,
+}
 
-    println!("{}", serde_json::to_string_pretty(&tree).unwrap());
+impl ProjectArgs {
+    fn get_project_path(&self) -> PathBuf {
+        let mut path = self.dir.clone();
+        path.push(to_kebab_case(self.project.as_str()));
+        path.with_extension("json")
+    }
+}
+
+#[derive(Parser)]
+#[command(group(clap::ArgGroup::new("formatting").args(&["indent", "min"]).multiple(false)))]
+struct PrintJsonArgs {
+    #[clap(flatten)]
+    project: ProjectArgs,
+    /// Number of spaces to indent the JSON output
+    #[arg(short, long, default_value_t = 2)]
+    indent: usize,
+    /// Minify the JSON output
+    #[arg(long)]
+    min: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a new project
+    New(ProjectArgs),
+    CreateDummy(ProjectArgs),
+    Tui(ProjectArgs),
+    PrintJson(PrintJsonArgs),
+}
+
+fn to_kebab_case(s: &str) -> String {
+    s.to_lowercase().replace(' ', "-").replace("_", "-")
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_kebab_case() {
+        assert_eq!(to_kebab_case("Hello World"), "hello-world");
+        assert_eq!(to_kebab_case("hello_world"), "hello-world");
+        assert_eq!(to_kebab_case("Rust Programming"), "rust-programming");
+        assert_eq!(to_kebab_case("Debug Tree CLI"), "debug-tree-cli");
+    }
+}
+
+fn main() {
+    let args = Cli::parse();
+    match &args.command {
+        Commands::New(args) => {
+            let tree: TreeType = Tree::new(Some(args.project.as_str()));
+            let project_path = args.get_project_path();
+            if project_path.exists() {
+                println!("Project already exists at {}", project_path.display());
+                return;
+            }
+
+            save_tree_to_file(&tree, &project_path).expect("Failed to save new project tree");
+            println!("Created empty project at {}", project_path.display());
+        }
+        Commands::CreateDummy(args) => {
+            let project_path = args.get_project_path();
+            if project_path.exists() {
+                println!("Project already exists at {}", project_path.display());
+                return;
+            }
+            let tree = get_dummy_tree(&args.project).expect("Failed to create dummy tree");
+            save_tree_to_file(&tree, &project_path).expect("Failed to save dummy project tree");
+            println!("Created dummy project at {}", project_path.display());
+        }
+        Commands::PrintJson(args) => {
+            let tree = load_tree_from_file(&args.project.get_project_path())
+                .expect("Failed to load project tree");
+            if args.min {
+                println!("{}", serde_json::to_string(&tree).unwrap());
+            } else {
+                let indent_vec = " ".repeat(args.indent).into_bytes();
+                let formatter = serde_json::ser::PrettyFormatter::with_indent(&indent_vec);
+                let mut buf = Vec::new();
+                let mut ser = serde_json::Serializer::with_formatter(&mut buf, formatter);
+                tree.serialize(&mut ser).expect("Failed to serialize tree");
+                println!(
+                    "{}",
+                    String::from_utf8(buf).expect("Failed to convert buffer to string")
+                );
+            }
+        }
+        Commands::Tui(_args) => {
+            todo!("Implement TUI functionality");
+        }
+    };
 }
