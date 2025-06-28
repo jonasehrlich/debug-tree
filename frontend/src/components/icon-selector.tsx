55c18529e43cd76b635bedab4425054a1a16402b
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-06-28 17:50:58 +0200

    fix(icon-selector): Add SelectTriggerIcon component
    This component can be used as a select trigger, but is simply an icon without text

--- frontend/src/components/icon-selector.tsx
+++ frontend/src/components/icon-selector.tsx
@@ -1,12 +1,11 @@
-import { cn } from "@/lib/utils";
 import {
   Select,
   SelectContent,
   SelectItem,
-  SelectTrigger,
-} from "@radix-ui/react-select";
+  SelectTriggerIcon,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
 import React from "react";
-import { Button } from "./ui/button";
 
 // A generic string key maps to a ReactNode (JSX.Element), these are the icons shown for the individual elements
 export type IconMap<T extends string = string> = Record<T, React.ReactNode>;
@@ -38,20 +37,15 @@ export const IconSelector = <T extends string>({
 }: IconSelectorProps<T>) => {
   return (
     <Select onValueChange={onSelectChange} value={selectedIcon}>
-      <SelectTrigger
-        className={cn(
-          "p-0 flex items-center justify-center",
-          "border-none focus:ring-0 focus:ring-offset-0",
-          "data-[state=open]:ring-0 data-[state=open]:ring-offset-0",
-          "cursor-pointer",
-        )}
+      <SelectTriggerIcon
+        variant="ghost"
+        size="sm"
+        // That it fits with a NodeHeaderAction
+        className="size-6 p-1"
         aria-label={ariaLabel ?? "Change Icon"}
-        asChild
       >
-        <Button variant="ghost" size="icon" className="nodrag size-6 p1">
-          {availableIcons[selectedIcon]}
-        </Button>
-      </SelectTrigger>
+        {availableIcons[selectedIcon]}
+      </SelectTriggerIcon>
       <SelectContent position="popper">
         {iconChoices.map((option) => (
           <SelectItem
@@ -59,13 +53,8 @@ export const IconSelector = <T extends string>({
             value={option.value}
             className="p-0 bg-popover"
           >
-            <div
-              className={cn(
-                "flex items-center gap-2 px-3 py-1 rounded-md bg-popover hover:bg-muted",
-                "focus:bg-muted w-full transition-colors text-sm hover:border-none",
-              )}
-            >
-              <span className="shrink-0">
+            <div className={cn("flex items-center gap-2 px-3 py-2")}>
+              <span>
                 {availableIcons[option.value]}{" "}
                 {/* Get the icon for this option's value */}
               </span>
