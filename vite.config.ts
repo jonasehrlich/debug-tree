d9a7bcc3beae1698b0a29283d4a37c0d451ed1dc
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-07-16 22:37:57 +0200

    chore: Add build:analyze to analyze the bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
+const shouldAnalyze = process.env.ANALYZE_ROLLUP === "true";
 // https://vite.dev/config/
 export default defineConfig({
   root: "./frontend",
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      plugins: shouldAnalyze
+        ? [
+            visualizer({
+              open: true,
+              gzipSize: true,
+              brotliSize: true,
+              filename: path.resolve(__dirname, "frontend/dist/stats.html"),
+            }),
+          ]
+        : [],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./frontend/src"),
