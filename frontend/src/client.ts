8aff1da0bbacd2244ac94c70435063dd990cf036
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-07-16 18:47:23 +0200

    feat(api-client): Add logging middleware

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,4 +1,38 @@
-import createClient from "openapi-fetch";
+import log from "loglevel";
+import createClient, { type Middleware } from "openapi-fetch";
 import type { paths } from "./types/api";
 
+const logger = log.getLogger("api-client");
+
+/**
+ * Middleware for the API client to log all requests and their params
+ */
+const loggerMiddleware: Middleware = {
+  async onRequest({ request, params, schemaPath }) {
+    if (logger.getLevel() > log.levels.DEBUG) {
+      // Exit early if the logger's log level is larger than DEBUG.
+      // No need to parse the request if it is anyways not logged
+      return;
+    }
+    const args: unknown[] = [];
+    if (Object.hasOwn(params, "query")) {
+      args.push("queryParams:", params.query);
+    }
+    if (Object.hasOwn(params, "path")) {
+      args.push("pathParams:", params.path);
+    }
+    if (
+      request.method === "POST" &&
+      request.headers.get("content-type")?.includes("application/json") &&
+      request.body !== null
+    ) {
+      const body: unknown = await request.clone().json();
+      args.push("body:", body);
+    }
+
+    logger.debug(request.method, schemaPath, ...args);
+  },
+};
+
 export const client = createClient<paths>({ baseUrl: "/" });
+client.use(loggerMiddleware);
