b2f35068a73433488270084cad3d1a546629a562
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-07-18 00:06:17 +0200

    build: Add react-markdown and tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"], // This is important!
+  darkMode: "class", // This is important!
   content: ["./frontend/index.html", "./frontend/src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
