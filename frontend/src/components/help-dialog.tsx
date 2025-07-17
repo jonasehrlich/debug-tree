76ccd742e09bdf1dd582231cfb9a1f7c957e4700
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-07-18 00:41:51 +0200

    feat: Style help dialog only using prose classes
    Also use `<kbd />` component to show keyboard shortcuts.

--- frontend/src/components/help-dialog.tsx
+++ frontend/src/components/help-dialog.tsx
@@ -12,29 +12,11 @@ import type { UiState } from "@/types/state";
 import { Pin } from "lucide-react";
 import { useShallow } from "zustand/react/shallow";
 import { Button } from "./ui/button";
-import { Separator } from "./ui/separator";
 
 const uIStoreSelector = (s: UiState) => ({
   isHelpDialogOpen: s.isHelpDialogOpen,
   setIsHelpDialogOpen: s.setIsHelpDialogOpen,
 });
-
-const HelpSection = ({
-  title,
-  content,
-}: {
-  title: string;
-  content: React.ReactNode;
-}) => {
-  return (
-    <div className="flex flex-col gap-4 border-accent">
-      <h3 className="font-semibold">{title}</h3>
-
-      <div className="text-muted-foreground">{content}</div>
-      <Separator />
-    </div>
-  );
-};
 
 export const HelpDialog = () => {
   const { isHelpDialogOpen, setIsHelpDialogOpen } = useUiStore(
@@ -44,72 +26,51 @@ export const HelpDialog = () => {
   return (
     <Dialog open={isHelpDialogOpen} onOpenChange={setIsHelpDialogOpen}>
       <DialogContent
-        className="sm:max-w-[550px]"
+        className="sm:max-w-2xl prose"
         aria-description="Help"
         aria-describedby={undefined}
       >
         <DialogHeader>
           <DialogTitle>Help</DialogTitle>
         </DialogHeader>
-        <HelpSection
-          title="Create or open flow"
-          content={
-            <div>
-              <p>
-                Create a new debug-flow by either clicking the project entry in
-                the menu bar or select <i>File &gt; Open / Create Flow</i>.
-              </p>
-              <p>
-                This opens a new dialog which allows opening an existing flow or
-                creating a new one.
-              </p>
-            </div>
-          }
-        />
-        <HelpSection
-          title="Create a node"
-          content={
-            <p>
-              Create a node by pulling the edge from the handle of a node. This
-              will create a pop-up to fill the initial node data and create the
-              node.
-            </p>
-          }
-        />
-        <HelpSection
-          title="Edit a node"
-          content={
-            <p>
-              Either double-click on a node or select <i>Edit</i> from the node
-              header ellipsis menu.
-            </p>
-          }
-        />
-        <HelpSection
-          title="Delete a node"
-          content={
-            <p>
-              Create a node by pressing <i>Delete</i> from the node header
-              ellipsis menu. Or select a node and press the <i>Backspace</i>
-              key.
-            </p>
-          }
-        />
+        <h4>Create or open flow</h4>
+        <p>
+          Create a new debug-flow by either clicking the project entry in the
+          menu bar or select <i>File &gt; Open / Create Flow</i>.
+        </p>
+        <p>
+          This opens a new dialog which allows opening an existing flow or
+          creating a new one.
+        </p>
 
-        <HelpSection
-          title="Show Git Graph"
-          content={
-            <p>
-              For <i>StatusNodes</i> which have a Git reference assigned to
-              them, click the{" "}
-              <Pin className="inline bg-accent rounded-sm" size="1em" /> icon in
-              the footer of the node to pin the revision. A panel will appear in
-              the lower-right corner of the screen. After a second revision is
-              pinned, teh Git Graph can be displayed by clicking on the{" "}
-              <i>Show Git Graph</i> button.
-            </p>
-          }
-        />
+        <h4>Create a node</h4>
+        <p>
+          Create a node by pulling the edge from the handle of a node. This will
+          create a pop-up to fill the initial node data and create the node.
+        </p>
+
+        <h4>Edit a node</h4>
+        <p>
+          Either double-click on a node or select <i>Edit</i> from the node
+          header ellipsis menu.
+        </p>
+
+        <h4>Delete a node</h4>
+        <p>
+          Create a node by pressing <i>Delete</i> from the node header ellipsis
+          menu. Or select a node and press the <i>Backspace</i> key.
+        </p>
+
+        <h4>Show Git Graph</h4>
+        <p>
+          For <i>StatusNodes</i> which have a Git reference assigned to them,
+          click the <Pin className="inline bg-accent rounded-sm" size="1em" />{" "}
+          icon in the footer of the node to pin the revision. A panel will
+          appear in the lower-right corner of the screen. After a second
+          revision is pinned, teh Git Graph can be displayed by clicking on the{" "}
+          <i>Show Git Graph</i> button.
+        </p>
+
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
@@ -134,7 +95,7 @@ export const KeybindingsDialog = () => {
       onOpenChange={setIsKeybindingsDialogOpen}
     >
       <DialogContent
-        className="sm:max-w-[550px]"
+        className="sm:max-w-2xl"
         aria-description="Keybindings"
         aria-describedby={undefined}
       >
@@ -146,9 +107,9 @@ export const KeybindingsDialog = () => {
           {Object.entries(keybindings).map(([key, value]) => {
             return (
               <div key={key} className="flex space-x-3 items-center">
-                <span className="font-mono px-2 py-1 bg-accent rounded-md min-w-12 text-center text-sm">
+                <kbd className="px-2 py-1 bg-accent rounded-md min-w-12 text-center text-sm">
                   {value.repr}
-                </span>
+                </kbd>
                 <div>{value.description}</div>
               </div>
             );
