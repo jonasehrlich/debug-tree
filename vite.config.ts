fab3fe780a5b022af1c2159aa3135fbbe88e8dbe
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-07-21 19:31:13 +0200

    feat: Add test:coverage command

--- vite.config.ts
+++ vite.config.ts
@@ -34,10 +34,11 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./vitest.setup.ts",
-    //  browser: {
-    //     enabled: true,
-    //     provider: "playwright",
-    //     instances: [{ browser: "chromium" }],
-    //   },
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
 });
